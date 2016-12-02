33ffdf11a35341e097d25fa64047d251e9b5be1a
Andre Fohlin <andre.fohlin@king.com>
2016-12-02 17:20:24 +0100

    Solved day 2 part 1

--- day 2/main.js
+++ day 2/main.js
@@ -7,3 +7,42 @@ var fifth =
 "DLLLRDDURDULRRLULURRDULDLUDLURDDURRLLRRLLULRDLDRDULRLLRDRUUULURRRLLRLDDDRDRRULDRRLLLLDLUULRRRURDDRULLULDDDLULRLRRRUDRURULUDDRULDUDRLDRRLURULRUULLLRUURDURLLULUURUULUUDLUDLRRULLLRRLRURDRRURDRULRURRUDUDDDRDDULDLURUDRDURLDLDLUDURLLRUULLURLDDDURDULRLUUUDLLRRLLUURRDUUDUUDUURURDRRRRRRRRRUDULDLULURUDUURDDULDUDDRDDRDRLRUUUUDLDLRDUURRLRUUDDDDURLRRULURDUUDLUUDUUURUUDRURDRDDDDULRLLRURLRLRDDLRUULLULULRRURURDDUULRDRRDRDLRDRRLDUDDULLDRUDDRRRD";
 
 
+var keypad = [  [1,2,3],
+                [4,5,6],
+                [7,8,9]  ];
+
+var currentPos = {x: 1, y: 1};
+
+var combination = [];
+combination.push(figureOutKeyBasedOnSequence(first));
+combination.push(figureOutKeyBasedOnSequence(second));
+combination.push(figureOutKeyBasedOnSequence(third));
+combination.push(figureOutKeyBasedOnSequence(fourth));
+combination.push(figureOutKeyBasedOnSequence(fifth));
+
+console.log(combination);
+
+function figureOutKeyBasedOnSequence(sequence) {
+    for(var i = 0; i < sequence.length; ++i) {
+        var char = sequence.charAt(i);
+        switch(char){
+            case 'U':
+                currentPos.y = clamp(currentPos.y-1, 0, 2);
+            break;
+            case 'D':
+                currentPos.y = clamp(currentPos.y+1, 0, 2);
+            break;
+            case 'L':
+                currentPos.x = clamp(currentPos.x-1, 0, 2);
+            break;
+            case 'R':
+                currentPos.x = clamp(currentPos.x+1, 0, 2);
+            break;
+        }
+    }
+    return keypad[currentPos.y][currentPos.x];
+}
+
+function clamp(val, min, max){
+    return Math.max(min, Math.min(val, max));
+}
